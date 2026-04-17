4efbee6e895bf956312865a8a9855e51993a73c6
Rakshi <rakshithat004@gmail.com>
2026-04-17 11:59:02 +0530

    New Branch Commit

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        println("Hello World");
+    }
+    public static void println(String message){
+        System.out.println(message);
     }
 }
